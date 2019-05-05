15aca9bd63614ff636b3935c85717a2366ad925d
matrixsmaster <matrixsmaster@yandex.ru>
2019-05-05 04:15:01 +0200

    MSM: version bump

--- components/version/version.hpp
+++ components/version/version.hpp
@@ -11,7 +11,7 @@ namespace Version
     	// I have to hard-code it because the version resource file in my setup is too volatile
     	// and could completely be out of sync with the current ELF file being executed
         const std::string mVersion = "0.46.0-pre";
-        const std::string mCommitHash = "9f7f88af193d6e77ab0569e741b25d99b905a3fa"; // this would point to the PREVIOUS commit
+        const std::string mCommitHash = "f0b95b71a1927b73a5136bc6c4792e2a90ecb83d"; // this would point to the PREVIOUS commit
 
         std::string describe();
     };
