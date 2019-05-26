616dc235edc0a77acb48ef9c88ae93173dc39aa5
matrixsmaster <matrixsmaster@yandex.ru>
2019-05-26 23:40:03 +0200

    MSM: version bump after merge branch ocs/newfilter

--- components/version/version.hpp
+++ components/version/version.hpp
@@ -11,7 +11,7 @@ namespace Version
     	// I have to hard-code it because the version resource file in my setup is too volatile
     	// and could completely be out of sync with the current ELF file being executed
         const std::string mVersion = "0.45.0";
-        const std::string mCommitHash = "5b88d3da0c5c34a76c467a8dc88dcffab5a38248"; // this would point to the PREVIOUS commit
+        const std::string mCommitHash = "57e2e374e005fb3b3418f60d05fd0a8b0fdbd387"; // this would point to the PREVIOUS commit
 
         std::string describe();
     };
